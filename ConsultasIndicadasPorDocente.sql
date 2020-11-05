d821395baaa76bf63adc47fdee582b322ecfc67a
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-11-04 23:31:47 -0300

    Update ConsultasIndicadasPorDocente.sql

--- ConsultasIndicadasPorDocente.sql
+++ ConsultasIndicadasPorDocente.sql
@@ -30,7 +30,7 @@ WHERE t.idSintoma  IS NULL;
 
 
 
-4) --Mostrar el ID, Nombre y Apellido de los médicos que mas consultas atendieron los últimos 30 días
+--4) --Mostrar el ID, Nombre y Apellido de los médicos que mas consultas atendieron los últimos 30 días
 
 SELECT  c.docidentidadMedico AS Id, p.nombres AS Nombre, p.apellidos  AS Apellido, count(DISTINCT c.idSesion ) AS Cantidad_De_Consultas
 FROM  chat c
@@ -41,7 +41,7 @@ ORDER BY COUNT(c.idSesion ) DESC
 LIMIT 10;
 
 
-5) Mostrar el ID y Nombre de Enfermedades de los últimos 15 días que hayan sido diagnosticadas, con mas de 3 síntomas
+--5) Mostrar el ID y Nombre de Enfermedades de los últimos 15 días que hayan sido diagnosticadas, con mas de 3 síntomas
 
 SELECT  p.id AS Id_Patologia, p.nombre AS Nombre_Patologia, COUNT(DISTINCT a.IdSintoma ) AS Cantidad_De_Sintomas
 FROM  asociados a
@@ -55,7 +55,7 @@ ORDER BY COUNT(a.idSintoma) DESC
 LIMIT 10;
 
 
-6)Mostrar el ID, Nombre y Apellido de los usuarios que hayan tenido mas de 10 consultas los últimos 20 días, con los síntomas "Fiebre" y "Dolor de Cabeza"
+--6)Mostrar el ID, Nombre y Apellido de los usuarios que hayan tenido mas de 10 consultas los últimos 20 días, con los síntomas "Fiebre" y "Dolor de Cabeza"
 
 
 SELECT r.idPaciente ID, nombres Nombres, apellidos Apellidos from recibe r 
@@ -68,9 +68,9 @@ GROUP BY idPaciente
 HAVING count(r.idPaciente ) > 10;
 
 
-Opcionales 
+--Opcionales 
 
-Mostrar el ID y Fecha de inicio de las sesiones de chat con más de 40 mensajes
+--Mostrar el ID y Fecha de inicio de las sesiones de chat con más de 40 mensajes
 
 SELECT c1.idSesion,DATE_FORMAT( c1.fechaHora ,'%d/%m/%Y') AS Fecha, count( DISTINCT c1.mensaje ) AS cantidad
 FROM chat c1
@@ -81,7 +81,7 @@ HAVING COUNT(DISTINCT c1.mensaje )>40
 ORDER by count(c1.idSesion )=1 DESC;
 
 
-Mostrar el ID, Nombre y Apellido de los médicos que no hayan atendido ninguna consulta
+--Mostrar el ID, Nombre y Apellido de los médicos que no hayan atendido ninguna consulta
 
 SELECT  p.docidentidad AS Id, p.nombres AS Nombre, p.apellidos  AS Apellido
 FROM  persona p
@@ -90,7 +90,7 @@ INNER JOIN medico  m ON p.docidentidad = m.docidentidad
 WHERE c.idSesion  IS NULL;
 
 
-Mostrar el ID de las enfermedades que no se hayan diagnosticado nunca.
+--Mostrar el ID de las enfermedades que no se hayan diagnosticado nunca.
 
 
 SELECT p.id, p.nombre
@@ -98,5 +98,5 @@ FROM patologia p
 LEFT JOIN tiene t ON p.id = t.idPatologia 
 WHERE t.idPatologia  IS NULL;
 
-FIN
+--FIN
 
